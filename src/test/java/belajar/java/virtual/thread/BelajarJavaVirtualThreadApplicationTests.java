c34d05a62cdb9b4a335c499dd7c59a37d4409066
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-25 10:41:06 +0700

    Platform Thread, Initial commit

---
+++ src/test/java/belajar/java/virtual/thread/BelajarJavaVirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.java.virtual.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaVirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
